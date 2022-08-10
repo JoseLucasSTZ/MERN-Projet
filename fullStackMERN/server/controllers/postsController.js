96f3129fffce4e2bb683dd75ff935c83a6d6e2df
José Lucas Steinmetz da Silveira <jls.silveira@outlook.com>
2022-08-10 14:11:28 -0300

    update
    update

--- fullStackMERN/server/controllers/postsController.js
+++ fullStackMERN/server/controllers/postsController.js
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose'
 import PostMessage from '../models/postMessage.js'
 
 export const getPosts = async (req, res) => {
@@ -25,4 +26,15 @@ export const createPost = async (req, res) => {
     } catch (error) {
         res.status(409).json(message(error.message))
     }
+}
+
+export const updatePost = async (req, res) => {
+    const { _id } = req.params
+    const post = req.body
+
+    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('no post with that ID')
+     
+    const updatedPost = await PostMessage.findByIdAndDelete(_id, post, {new:true})
+
+    res.json(updatedPost)
 }
