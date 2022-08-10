94a7b2fe8c82661668575d915ea46935561c806c
José Lucas Steinmetz da Silveira <jls.silveira@outlook.com>
2022-08-10 14:27:45 -0300

    update
    update

---
+++ fullStackMERN/ladoCliente/src/reducers/posts.js
@@ -0,0 +1,14 @@
+import Posts from "../components/Posts/Posts"
+
+export default (posts = [], action) => {
+    switch(action.type) {
+        case 'UPDATE':
+            return Posts.map((post) => post.id === action.payload._id ? action.payload:post)
+        case 'FETCH_ALL':
+            return action.payload
+        case 'CREATE':
+            return [ ...posts, action.payload ]
+        default:
+            return posts
+    }
+}
